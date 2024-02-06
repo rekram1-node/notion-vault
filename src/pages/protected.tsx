2cc6219e6158c9ddd7f0a70778aa13300380bbe6
Aiden Cline <aidenpcline@gmail.com>
2024-02-06 10:52:33 -0600

    add eye icon to password page

--- src/pages/protected.tsx
+++ src/pages/protected.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { EyeOpenIcon, EyeNoneIcon } from '@radix-ui/react-icons';
 
 const ProtectedPage = () => {
   const [password, setPassword] = useState('');
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [content, setContent] = useState('Encrypted content from server');
   const [error, setError] = useState(false);
+  const [eyeOpen, setEyeOpen] = useState(true);
 
   // Placeholder for decryption function
   const decryptContent = (encryptedContent: string) => {
@@ -25,6 +27,10 @@ const ProtectedPage = () => {
     }
   };
 
+  const handleEyeClick = () => {
+    setEyeOpen(!eyeOpen);
+  }
+
   return (
     <div className="absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
       <div className="flex justify-center items-center h-screen">
@@ -32,14 +38,21 @@ const ProtectedPage = () => {
           {!isAuthenticated ? (
             <div className="flex flex-col space-y-4">
               <p className="text-center font-normal">Your content is locked. Verify your master password to continue</p> {/* << May want to increase font */}
-              <input
-                type="password"
-                placeholder="Enter Master Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="border p-2 rounded "
-              />
-              {/* ADD HIDDEN INPUT TOGGLE BUTTON */}
+              <div className="flex items-center">
+                <input
+                  type={eyeOpen ? "password" : "text"}
+                  placeholder="Enter Master Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="border p-2 rounded flex-grow"
+                />
+                <button
+                  onClick={handleEyeClick}
+                  className="ml-2" // Add some left margin to separate it from the input
+                >
+                  {eyeOpen ? <EyeOpenIcon className="w-5 h-5"/> : <EyeNoneIcon className="w-5 h-5"/>}
+                </button>
+              </div>
               {error && <p className="text-red-500 text-sm text-center">Incorrect password, please try again.</p>}
               <button
                 onClick={handlePasswordSubmit}
