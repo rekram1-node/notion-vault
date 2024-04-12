ebf6d25c869d6732b0c44f92276fdcd365232451
Aiden Cline <aidenpcline@gmail.com>
2024-04-11 22:40:15 -0500

    fixing webhook verification

--- src/pages/api/clerk.ts
+++ src/pages/api/clerk.ts
@@ -1,8 +1,15 @@
+import { Webhook } from "svix";
 import { type WebhookEvent } from "@clerk/nextjs/server";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Notion } from "~/server/notionIntegration";
 import { prisma } from "~/server/db";
-import crypto from "crypto";
+import { buffer } from "micro";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 type ResponseData = {
   message: string;
@@ -12,16 +19,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>,
 ) {
-  switch (req.method) {
-    case "GET":
-      res.status(200).json({ message: "Hello from Next.js!" });
-      break;
-    case "POST":
-      await handleClerkWebhook(req, res);
-      break;
-    default:
-      res.status(400).json({ message: `Unsupported Method: ${req.method}` });
+  if (req.method !== "POST") {
+    return res.status(405);
   }
+  await handleClerkWebhook(req, res);
 }
 
 async function handleClerkWebhook(
@@ -29,7 +30,7 @@ async function handleClerkWebhook(
   res: NextApiResponse<ResponseData>,
 ) {
   try {
-    if (!validSignature(req)) {
+    if (!(await validSignature(req))) {
       return res.status(400).json({
         message: "only valid signed events from clerk will be accepted",
       });
@@ -101,35 +102,40 @@ async function handleClerkWebhook(
   }
 }
 
-function validSignature(req: NextApiRequest) {
+async function validSignature(req: NextApiRequest) {
   try {
-    const svixSignature = req.headers["svix-signature"];
-    if (!svixSignature) {
-      return false;
+    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+    if (!WEBHOOK_SECRET) {
+      throw new Error(
+        "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
+      );
     }
-    if (typeof svixSignature !== "string") {
-      return false;
-    }
-    const svixId = req.headers["svix-id"];
-    if (typeof svixId !== "string") {
-      return false;
-    }
-    const svixTimestamp = req.headers["svix-timestamp"];
-    if (typeof svixTimestamp !== "string") {
+
+    const svix_id = req.headers["svix-id"] as string;
+    const svix_timestamp = req.headers["svix-timestamp"] as string;
+    const svix_signature = req.headers["svix-signature"] as string;
+
+    if (!svix_id || !svix_timestamp || !svix_signature) {
       return false;
     }
 
-    const signedContent = `${svixId}.${svixTimestamp}.${req.body}`;
-    const secret = process.env.CLERK_WEBHOOK_SIGNING_SECRET ?? "";
-    const secretBytes = new Buffer(secret.split("_")[1] ?? "", "base64");
-    const signature = crypto
-      .createHmac("sha256", secretBytes)
-      .update(signedContent)
-      .digest("base64");
+    const body = (await buffer(req)).toString();
 
-    if (signature !== svixSignature) {
-      return false;
-    }
+    // Create a new Svix instance with your secret.
+    const wh = new Webhook(WEBHOOK_SECRET);
+
+    const evt: WebhookEvent = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+
+    const { id } = evt.data;
+    const eventType = evt.type;
+
+    console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+    console.log("Webhook body:", body);
 
     return true;
   } catch (_e) {
