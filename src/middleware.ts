7c11424561ef45b38e6080724c3f73d004256f2a
Aiden Cline <aidenpcline@gmail.com>
2024-07-06 22:25:46 -0500

    remove unused code blocks and pointless eslint ignore comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/"]);
